827e4ff7b3084fd25b5fac8c679ab5f73c67b651
Ritesh Dhyani <dhyaniritesh89@gmail.com>
2026-04-06 09:57:46 +0530

    MVP created

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,5 +1,14 @@
-export default function Preview() {
+export default function Preview({ data }) {
     return <>
-        <h2>gawar</h2>
+        <h1>{data.personalInfo.firstName}{data.personalInfo.lastName}</h1>
+        <p>{data.personalInfo.email}</p>
+        <h2>Education</h2>
+
+        {Object.entries(data.education).map(([id, value]) => (
+            <div key={id}>
+                <h3>{value.schoolName}</h3>
+                <p>{value.degree}</p>
+            </div>
+        ))}
     </>
 }
